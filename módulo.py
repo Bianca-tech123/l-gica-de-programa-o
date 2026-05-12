e04eef14ce10c4fa007850989b33b4c5d0702ca8
Bianca-tech123 <bianca.r.rodrigues1213@gmail.com>
2026-05-12 10:41:17 -0300

    Add files via upload

---
+++ módulo.py
@@ -0,0 +1,10 @@
+def soma(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multiplicação(a,b):
+    return a*b
+def divisão(a,b):
+    return a/b
+def limpa_Teminal():
+ os.system('cls'if os.name == 'nt' else 'clear' )
